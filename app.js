95bb3d960d7a00b613edfdf73caa49bb2d7f8cfc
Kavish Jain <kavishme@gmail.com>
2016-05-24 17:31:24 -0700

    mongo driver

--- app.js
+++ app.js
@@ -24,7 +24,22 @@ var crypto = require('crypto');
 var fs = require('fs');
 var express = require('express');
 var Client = require('node-rest-client').Client;
+var MongoClient = require('mongodb').MongoClient;
 
+var connectionString = "mongodb://52.40.82.144," +
+    "52.40.129.76," +
+    "52.38.117.207" +
+    "/gumball" +
+    "?readPreference=secondary&" +
+    "w=2&" +
+    "wtimeoutMS=2000&" +
+    "connectTimeoutMS=5000&" +
+    "replicaSet=kjCluster";
+
+var assert = require("assert");
+var modelNumber = 2;
+var serialNumber = 1234;
+var options = {server: {reconnectInterval:50, reconnectTries:5}};
 var app = express();
 app.use(express.bodyParser());
 app.use("/images", express.static(__dirname + '/images'));
@@ -41,8 +56,7 @@ var get_hash = function( state, ts ) {
     hmac.end() ;
     hash = hmac.read();
     //console.log( "HASH: " + hash )
-    return hash ;
-
+    return hash;
 }
 
 
@@ -64,7 +78,6 @@ var error = function( req, res, msg, ts ) {
 }
 
 
-
 var page = function( req, res, state, ts ) {
 
     body = fs.readFileSync('./gumball.html');
@@ -74,56 +87,128 @@ var page = function( req, res, state, ts ) {
     hash = get_hash ( state, ts ) ;
     console.log( state ) ;
 
-    var client = new Client();
-            var count = "";
-            client.get( endpoint,
-                function(data, response_raw){
-                    //console.log("in page get");
-                    console.log(data);
-                    data = JSON.parse(data);
-                    count = data.countGumballs
-                    //console.log( data["countGumballs"] + " count = " + count ) ;
-                    var msg =   "\n\nMighty Gumball, Inc.\n\nNodeJS-Enabled Standing Gumball\nModel# " +
-                                data.modelNumber + "\n" +
-                                "Serial# " + data.serialNumber + "\n" +
-                                "\n" + state +"\n\n" ;
-                    var html_body = "" + body ;
-                    var html_body = html_body.replace("{message}", msg );
-                    var html_body = html_body.replace("{ts}", ts );
-                    var html_body = html_body.replace("{hash}", hash );
-                    var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
-                    res.end( html_body );
-            });
+    // var client = new Client();
+    //         var count = "";
+    //         client.get( endpoint,
+    //             function(data, response_raw){
+    //                 //console.log("in page get");
+    //                 console.log(data);
+    //                 data = JSON.parse(data);
+    //                 count = data.countGumballs
+    //                 //console.log( data["countGumballs"] + " count = " + count ) ;
+    //                 var msg =   "\n\nMighty Gumball, Inc.\n\nNodeJS-Enabled Standing Gumball\nModel# " +
+    //                             data.modelNumber + "\n" +
+    //                             "Serial# " + data.serialNumber + "\n" +
+    //                             "\n" + state +"\n\n" ;
+    //                 var html_body = "" + body ;
+    //                 var html_body = html_body.replace("{message}", msg );
+    //                 var html_body = html_body.replace("{ts}", ts );
+    //                 var html_body = html_body.replace("{hash}", hash );
+    //                 var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
+    //                 res.end( html_body );
+    //         });
+
+
+    MongoClient.connect(connectionString, options, function(err, db) {
+        assert.equal(null, err);
+        assert.ok(db != null);
+
+        db.collection("machine").find( {serialNumber: serialNumber}).limit(1).toArray(function(err, docs) {
+            assert.equal(null, err);
+            assert.equal(1, docs.length);
+
+            console.log(docs);
+            data = docs[0];
+            db.close();
+
+            count = data.countGumballs
+            console.log( data["countGumballs"] + " count = " + count ) ;
+            var msg = "\n\nMighty Gumball, Inc.\n\nNodeJS-Enabled Standing Gumball\nModel# " +
+                        data.modelNumber + "\n" +
+                        "Serial# " + data.serialNumber + "\n" +
+                        "\n" + state +"\n\n" ;
+            var html_body = "" + body ;
+            var html_body = html_body.replace("{message}", msg );
+            var html_body = html_body.replace("{ts}", ts );
+            var html_body = html_body.replace("{hash}", hash );
+            var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
+            res.end( html_body );
+        });
+    });
 }
 
 var order = function( req, res, state, ts ) {
 
-    var client = new Client();
-            var count = 0;
-            client.get( endpoint,
-                function(data, response_raw) {
-                    data = JSON.parse(data);
-                    count = data.countGumballs ;
-                    console.log( "count before = " + count ) ;
-                    if ( count > 0 ) {
-                        count-- ;
-                        var args = {
-                            data: {"countGumballs": count, "serialNumber": data.serialNumber, "modelNumber": data.modelNumber},
-                            headers:{"Content-Type": "application/json"}
-                        };
-                        client.put( endpoint, args,
-                            function(data, response_raw) {
-                                console.log(data);
-                                console.log( "count after = " + data.countGumballs ) ;
-                                page( req, res, state, ts ) ;
-                            }
-                        );
-                    }
-                    else {
-                        error( req, res, "*** OUT OF INVENTORY ***", ts ) ;
-                    }
-            });
-}
+    // var client = new Client();
+    //         var count = 0;
+    //         client.get( endpoint,
+    //             function(data, response_raw) {
+    //                 data = JSON.parse(data);
+    //                 count = data.countGumballs ;
+    //                 console.log( "count before = " + count ) ;
+    //                 if ( count > 0 ) {
+    //                     count-- ;
+    //                     var args = {
+    //                         data: {"countGumballs": count, "serialNumber": data.serialNumber, "modelNumber": data.modelNumber},
+    //                         headers:{"Content-Type": "application/json"}
+    //                     };
+    //                     client.put( endpoint, args,
+    //                         function(data, response_raw) {
+    //                             console.log(data);
+    //                             console.log( "count after = " + data.countGumballs ) ;
+    //                             page( req, res, state, ts ) ;
+    //                         }
+    //                     );
+    //                 }
+    //                 else {
+    //                     error( req, res, "*** OUT OF INVENTORY ***", ts ) ;
+    //                 }
+    //         });
+
+    MongoClient.connect(connectionString, options, function(err, db) {
+        assert.equal(null, err);
+        assert.ok(db != null);
+
+        db.collection("machine").find( {serialNumber: serialNumber}).limit(1).toArray(function(err, docs) {
+            assert.equal(null, err);
+            assert.equal(1, docs.length);
+            console.log(docs);
+            data = docs[0];
+            //console.log("data: " + data);
+
+            count = data.countGumballs;
+            console.log( "count before = " + count ) ;
+            if ( count > 0 ) {
+                count-- ;
+                var data= {"countGumballs": count};
+
+                db.collection("machine").findOneAndUpdate({serialNumber: serialNumber},
+                    {$set: {countGumballs:count}}, 
+                    null,
+                    function (err, result) {
+                        assert.equal(null, err);
+                        assert.equal(result.ok, 1);
+                        //console.log(result);
+                        data = result.value;
+                        //console.log(data);
+                        console.log( "count after = " + data.countGumballs-1 );
+                        page( req, res, state, ts );
+                        db.close();
+                    });
+
+                // client.put( endpoint, args,
+                //     function(data, response_raw) {
+                //         console.log(data);
+                //         console.log( "count after = " + data.countGumballs ) ;
+                //         page( req, res, state, ts ) ;
+                //     });
+            }
+            else {
+                error( req, res, "*** OUT OF INVENTORY ***", ts ) ;
+            }
+        });
+    });
+};
 
 
 var handle_post = function (req, res) {
@@ -169,7 +254,7 @@ var handle_get = function (req, res) {
     state = "no-coin" ;
 
     page( req, res, state, ts ) ;
-}
+};
 
 app.set('port', (process.env.PORT || 5000));
 
